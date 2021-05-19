a6f339e89dfbf375e96ba32b7f8b96dd80cb7caf
Wyatt Simmons <simmonsw182@gmail.com>
2021-05-19 16:34:19 -0700

    created GET route for /notes.html

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.use(express.json());
 
 
 
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/notes.html'));
+});
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'));
 });
