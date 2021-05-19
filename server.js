33a0217354e008d5b58bc7270d2aec49a0c3aa11
Wyatt Simmons <simmonsw182@gmail.com>
2021-05-19 16:17:09 -0700

    added PORT, and parse incoming data

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
+app.use(express.static('public'));
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 
-app.listen(3001, () => {
-  console.log(`API server now on port 3001`);
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}`);
 });
