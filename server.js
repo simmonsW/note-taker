6ce12dcf5217b0e4b2fad106f673bf7dbbb680f1
Wyatt Simmons <simmonsw182@gmail.com>
2021-05-19 16:53:26 -0700

    created filterByQuery function, and GET route for api/notes

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 
+const { notes } = require('.db/db.json');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -11,7 +13,25 @@ app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 
+// FUNCTIONS
 
+function filterByQuery(query, notesArray) {
+  let filteredResults = notesArray;
+  if (query.title) {
+    filteredResults = filteredResults.filter(note => note.title === query.title);
+  }
+  return filteredResults;
+};
+
+// ROUTES
+
+app.get('/api/notes', (req, res) => {
+  let results = notes;
+  if (req.query) {
+    results = filterByQuery(req.query, results);
+  }
+  res.json(results);
+});
 
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/notes.html'));
